9a3411c54ea88019b2f5b0f2940a69f821b2ce82
Pablo <paul.ladiv123@gmail.com>
2018-12-22 11:14:40 +0000

    initial testing setup for react

---
+++ src/Components/button.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+class Button extends Component {
+    render () {
+        return (
+            <div>HEY THIS IS MY BUTTON COMPONENT</div>
+        )
+    }
+}
+
+export default Button
