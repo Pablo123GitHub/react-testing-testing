6013eca243330e4cd9ff0234797023f1e036d5be
Pablo <paul.ladiv123@gmail.com>
2018-12-22 11:25:01 +0000

    first basic test for button component

--- src/Components/button.js
+++ src/Components/button.js
@@ -1,6 +1,11 @@
 import React, {Component} from 'react'
 
 class Button extends Component {
+
+    state = {
+        text:""
+    }
+
     render () {
         return (
             <div>HEY THIS IS MY BUTTON COMPONENT</div>
--- src/Components/button.test.js
+++ src/Components/button.test.js
@@ -1,7 +1,16 @@
 import React from 'react'
 import Button from './button'
 import {shallow} from 'enzyme'
+import { create } from 'react-test-renderer'
 
-it('renders without crashing', ()=> {
+test('renders without crashing', ()=> {
     shallow(<Button/>)
 }) 
+
+describe("Button component", ()=>{
+    test("it shows the expected text when clicked (WRONG WAY)", ()=>{
+        const component = create(<Button text="I AM A BUTTON"/>)
+        const instance = component.getInstance();
+        expect(instance.state.text).toBe("")
+    })
+} )
